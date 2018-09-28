3bcc98db7d1490ae82f5b3f3c752177f2ec3d0a5
zedin27 <zeidtisnes@gmail.com>
2018-09-28 15:44:29 -0700

    ex01

--- d07/ex01/maxSW.c
+++ d07/ex01/maxSW.c
@@ -1,26 +1,17 @@
 #include "header.h"
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
 t_deque		*dequeInit(void)
 {
-	t_deque *queue;
+	t_deque *queue = malloc(sizeof(t_deque));
 
-	queue = (t_deque*)malloc(sizeof(t_queue));
+	// queue = (t_deque*)malloc(sizeof(t_queue));
 	queue->first = NULL;
 	queue->last = NULL;
-	return (node);
+	return (queue);
 }
-
-// int		isFull(t_deque *deque)
-// {
-// 	return (deque->value)
-// }
-//
-// int		isEmpty(t_deque *deque)
-// {
-//	return (deque->value)
-// }
 
 void	pushFront(struct s_deque *deque, int value)
 {
@@ -28,8 +19,8 @@ void	pushFront(struct s_deque *deque, int value)
 
 	node = (t_dequeNode*)malloc(sizeof(t_dequeNode));
 	node->value = value;
-	if (node == NULL)
-		return (NULL);
+	if (!node)
+		return ;
 	if (!deque->first)
 	{
 		node->next = NULL;
@@ -51,8 +42,8 @@ void	pushBack(struct s_deque *deque, int value)
 
 	node = (t_dequeNode*)malloc(sizeof(t_dequeNode));
 	node->value = value;
-	if (node == NULL)
-		return (NULL);
+	if (!node)
+		return ;
 	if (!deque->last)
 	{
 		node->prev = NULL;
@@ -62,16 +53,51 @@ void	pushBack(struct s_deque *deque, int value)
 	}
 	else
 	{
-
+		node->next = NULL;
+		node->prev = deque->last;
+		deque->last->next = node;
+		deque->last = node;
 	}
 }
-int		popFront(struct s_deque *deque)
+int	popFront(t_deque *deque)
 {
+	t_dequeNode *tmp;
+	int value;
 
+	if (!deque)
+		return INT_MIN;
+	if (!deque->first)
+		return INT_MIN;
+	value = deque->first->value;
+	tmp = deque->first;
+	deque->first = deque->first->next;
+
+	if (!deque->first)
+		deque->last = NULL;
+	else
+		deque->first->prev = NULL;
+	free(tmp);
+	return value;
 }
-int		popBack(struct s_deque *deque)
-{
 
+int popBack(t_deque *deque)
+{
+	t_dequeNode *tmp;
+	int value;
+
+	if (!deque)
+		return INT_MIN;
+	if (!deque->last)
+		return INT_MIN;
+	value = deque->last->value;
+	tmp = deque->last;
+	deque->last = deque->last->prev;
+	if (!deque->last)
+		deque->first = NULL;
+	else
+		deque->last->next = NULL;
+	free(tmp);
+	return value;
 }
 
 int		peekFront(t_deque *deque)
@@ -92,7 +118,33 @@ int		peekRear(t_deque *deque)
 	return (deque->first->value);
 }
 
-t_max	*maxSW(int *arr, int n, int k)
+t_max	*maxSlidingWindow(int *arr, int n, int k)
 {
+	t_deque *queue;
+	t_max *res;
+	int i;
 
+	queue = dequeInit();
+	res = (t_max*)malloc(sizeof(t_max));
+	res->max = (int*)malloc(sizeof(int)* (n - k + 1));
+	res->length = n - k + 1;
+	if (k > n || k < 0)
+		return (NULL);
+	for (i = 0; i < k; i++)
+	{
+		while (queue->last && peekFront(queue) != INT_MIN && arr[i] >= arr[peekFront(queue)])
+			popBack(queue);
+		pushBack(queue, i);
+	}
+	for (i = k; i < n; i++)
+	{
+		res->max[i - k] = arr[peekFront(queue)];
+		while (queue->first && peekRear(queue) != INT_MIN && peekRear(queue) <= i - k)
+			popFront(queue);
+		while (queue->last && peekFront(queue) != INT_MIN && arr[i] >= arr[peekFront(queue)])
+			popBack(queue);
+		pushBack(queue, i);
+	}
+	res->max[i - k] = arr[peekRear(queue)];
+	return (res);
 }
